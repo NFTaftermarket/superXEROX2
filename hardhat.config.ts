542eeca6b70587a745cbe632091ac4c448fee332
Ming-der Wang <mingderwang@gmail.com>
2021-08-14 00:06:51 +0800

    for CopyToken on bsctestnet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -122,6 +122,15 @@ export default {
     solidity: {
     compilers: [
       {
+        version: "0.7.0",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
+      },
+      {
         version: "0.7.6",
         settings: {
           optimizer: {
